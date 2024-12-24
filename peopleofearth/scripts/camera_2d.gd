6708a9a8318f814c1a5a8086622a53c9d9d3f6c8
Marin Dedvukaj <marin.dedvukaj2005@gmail.com>
2024-12-24 17:19:52 +0100

    basic game

---
+++ peopleofearth/scripts/camera_2d.gd
@@ -0,0 +1,12 @@
+extends Camera2D
+@export var follow_speed = 5.0
+@export var target: Node2D
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	if target and target is Node2D:
+		global_position = global_position.lerp(target.global_position, follow_speed * delta)
+	pass
